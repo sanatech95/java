27897337f77161dd5d199ae76a22e42f0d6837b1
sanatech95 <tasneem786786.khan@gmail.com>
2021-08-03 16:13:01 +0530

    write mutltiplication table Runner wdh eclips

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,10 @@
+package com.FirstJava;
+
+public class MultiplicationTableRunner {
+	
+	public static void main(String[] arg) {
+	
+	MultiplicationTable table=new MultiplicationTable();
+	table.print();
+}
+}
