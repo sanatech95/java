2373629f79c662348d45f248a38821318d0df9e5
sanatech95 <88230526+sanatech95@users.noreply.github.com>
2021-08-07 01:19:27 +0530

    Update MyNumber.java

--- java/Loops/src/com/loops/MyNumber.java
+++ java/Loops/src/com/loops/MyNumber.java
@@ -40,5 +40,19 @@ public class MyNumber {
 		}
 		return sum;
 	}
+	public void printNumberTraingle() {
 
+		//1
+		//1 2
+		//1 2 3
+		//1 2 3 4  
+		//1 2 3 4 5 
+		
+		for (int i=1; i <=number; i++) {
+			for (int j=1; j<=i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+			}
+		}
 }
